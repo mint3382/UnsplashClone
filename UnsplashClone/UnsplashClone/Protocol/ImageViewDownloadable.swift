1b16275f546aa59ef14479a659684467db7c2764
mint3382 <mskim0926@naver.com>
2024-01-30 19:18:53 +0900

    feat: ImageViewDownloadable 구현

---
+++ UnsplashClone/UnsplashClone/Protocol/ImageViewDownloadable.swift
@@ -0,0 +1,33 @@
+//
+//  ImageViewDownloadable.swift
+//  UnsplashClone
+//
+//  Created by minsong kim on 1/30/24.
+//
+
+import UIKit
+
+protocol ImageViewDownloadable {
+    var imageView: UIImageView { get set }
+}
+
+extension ImageViewDownloadable {
+    func downloadImage(url: URL) {
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data,
+                  let image = UIImage(data: data) else {
+                print("download error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            Task { @MainActor in
+                self.imageView.image = image
+            }
+        }
+        dataTask.resume()
+    }
+}
