3a8acad795917b8b743d278dba8173b6396b155d
mint3382 <mskim0926@naver.com>
2024-02-03 23:26:19 +0900

    refactor: RandomImageViewController를 collectionView에서 제거, 수정

--- UnsplashClone/UnsplashClone/Controller/RandomPhotoViewController.swift
+++ UnsplashClone/UnsplashClone/Controller/RandomPhotoViewController.swift
@@ -7,94 +7,209 @@
 
 import UIKit
 
-class RandomPhotoViewController: UIViewController {
-    private var photos: Photo?
+class RandomPhotoViewController: UIViewController, ImageViewDownloadable {
     private var photoItems: [PhotoElement] = []
-    {
-        didSet {
-            setSnapShot()
-        }
-    }
+    private var currentPage: Int?
     
-    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
-    private var dataSource: UICollectionViewDiffableDataSource<Int, PhotoElement>?
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var backView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = .init(gray: 0.8, alpha: 1)
+        view.layer.borderWidth = 2
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    var imageBackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    let closeButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "close2")?.resize(targetSize: CGSize(width: 40, height: 40))
+        image?.withTintColor(.white)
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(tappedPastButton), for: .touchUpInside)
+
+        button.setImage(image, for: .normal)
+        
+        return button
+    }()
+    
+    let checkButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "redBookmark")?.resize(targetSize: CGSize(width: 50, height: 50))
+        button.backgroundColor = .systemBackground
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(tappedNextButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let detailButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "information")?.resize(targetSize: CGSize(width: 40, height: 40))
+        button.backgroundColor = .systemBackground
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(tappedDetailButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.titleView = UIImageView(image: UIImage(named: "logo"))
-        loadPhotos()
-        configureCollectionView()
+        Task {
+            await loadPhotos()
+            configureImage()
+            configureImageUI()
+            configureStackView()
+            configureBackView()
+        }
     }
     
-    private func configureCollectionView() {
-        configureCollectionViewUI()
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(RandomPhotoCell.self, forCellWithReuseIdentifier: RandomPhotoCell.id)
-        collectionView.setCollectionViewLayout(createLayout(), animated: true)
-        setDataSource()
-        setSnapShot()
-    }
-    
-    private func setDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Int, PhotoElement>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RandomPhotoCell.id, for: indexPath) as? RandomPhotoCell else {
-                return UICollectionViewCell()
-            }
-            cell.configureImage(url: self.photoItems[indexPath.item].urls.regular)
-            cell.layer.shadowColor = UIColor.black.cgColor
-            cell.layer.shadowOpacity = 0.2
-            cell.layer.shadowRadius = 5
-            cell.layer.shadowOffset = CGSize(width: 2, height: 2)
-            return cell
-        })
-    }
-    
-    private func setSnapShot() {
-        var snapshot = NSDiffableDataSourceSnapshot<Int, PhotoElement>()
+    @objc func tappedDetailButton() {
+        guard let image = imageView.image,
+              let currentPage else {
+            return
+        }
         
-        snapshot.appendSections([0])
-        snapshot.appendItems(photoItems, toSection: 0)
-        
-        dataSource?.apply(snapshot)
+        let detailViewController: DetailViewController
+        let detailItem = DetailElement(
+            id: photoItems[currentPage].id,
+            title: photoItems[currentPage].title,
+            width: photoItems[currentPage].width,
+            height: photoItems[currentPage].height,
+            descriptions: photoItems[currentPage].description,
+            altDescription: photoItems[currentPage].altDescription,
+            urls: photoItems[currentPage].urls.regular.absoluteString,
+            likedByUser: PhotoService.shared.isBookmarkedData(photoItems[currentPage]),
+            user: photoItems[currentPage].user)
+        detailViewController = DetailViewController(item: detailItem, image: image)
+        self.present(detailViewController, animated: true)
     }
     
-    private func configureCollectionViewUI() {
-        view.addSubview(collectionView)
+    @objc func tappedNextButton() {
+        guard var current = currentPage else {
+            return
+        }
+        current += 1
+        
+        if current == photoItems.count {
+            currentPage = 0
+        } else {
+            currentPage = current
+        }
+        configureImage()
+        configureImageUI()
+    }
+    
+    @objc func tappedPastButton() {
+        guard var current = currentPage else {
+            return
+        }
+        current -= 1
+        
+        if current == -1 {
+            currentPage = photoItems.count - 1
+        } else {
+            currentPage = current
+        }
+        
+        configureImage()
+        configureImageUI()
+    }
+    
+    
+    
+    func configureImage() {
+        guard let currentPage else {
+            imageView.image = UIImage(named: "noImage")
+            return
+        }
+        
+        let item = photoItems[currentPage]
+        
+        let url = item.urls.regular
+        downloadImage(url: url)
+    }
+    
+    func configureBackView() {
+        backView.layer.shadowPath = UIBezierPath(roundedRect: backView.bounds, cornerRadius: backView.layer.cornerRadius).cgPath
+        backView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        backView.layer.shadowColor = UIColor.black.cgColor
+        backView.layer.shadowOpacity = 0.8
+        backView.layer.shadowRadius = 10
+    }
+    
+    private func configureImageUI() {
+        view.addSubview(backView)
+        backView.addSubview(imageBackView)
+        imageBackView.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 8)
+            backView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7),
+            backView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            backView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            backView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            imageBackView.topAnchor.constraint(equalTo: backView.topAnchor, constant: 12),
+            imageBackView.heightAnchor.constraint(equalTo: backView.heightAnchor, multiplier: 0.75),
+            imageBackView.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: 12),
+            imageBackView.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -12)
+        ])
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: imageBackView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: imageBackView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: imageBackView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: imageBackView.trailingAnchor)
         ])
     }
     
-    private func createLayout() -> UICollectionViewCompositionalLayout {
-
-        return UICollectionViewCompositionalLayout (sectionProvider: {[weak self] sectionIndex, _ in
-            
-            return self?.createRandomPhotoSection()
-        })
+    private func configureStackView() {
+        buttonStackView.addArrangedSubview(closeButton)
+        buttonStackView.addArrangedSubview(checkButton)
+        buttonStackView.addArrangedSubview(detailButton)
+        backView.addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: imageBackView.bottomAnchor, constant: 20),
+            buttonStackView.bottomAnchor.constraint(equalTo: backView.bottomAnchor, constant: -20),
+            buttonStackView.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: 30),
+            buttonStackView.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -30)
+        ])
     }
     
-    private func createRandomPhotoSection() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .fractionalHeight(0.9))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        group.interItemSpacing = .fixed(8)
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 8
-        section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 10, bottom: -5, trailing: 10)
-        section.orthogonalScrollingBehavior = .continuous
-        
-        return section
-    }
     
-    private func loadPhotos() {
+    private func loadPhotos() async {
         let endPoint = EndPoint(queries: nil)
         let apiProvider = APIProvider()
         guard let request = endPoint.configureRequest() else {
@@ -102,16 +217,14 @@ class RandomPhotoViewController: UIViewController {
             return
         }
         
-        Task {
-            do {
-                let photo = try await apiProvider.fetchData(decodingType: Photo.self, request: request)
-                photo.forEach { photoElement in
-                    photoItems.append(photoElement)
-                }
-            } catch {
-                //TODO: 실패 알럿
-                print(error.localizedDescription)
+        do {
+            let photo = try await apiProvider.fetchData(decodingType: Photo.self, request: request)
+            photo.forEach { photoElement in
+                photoItems.append(photoElement)
             }
+        } catch {
+            //TODO: 실패 알럿
+            print(error.localizedDescription)
         }
     }
 }
